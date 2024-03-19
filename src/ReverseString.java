81692951fb1b22295118859e1cd4c6702e02bd9a
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-03-19 15:42:21 +0530

    Reverse String

---
+++ src/ReverseString.java
@@ -0,0 +1,16 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String s1 = "I love programming";
+
+        String revStr = reverseStr(s1);
+        System.out.println(revStr);
+    }
+
+    public static String reverseStr(String a){
+        String finalString = "";
+        for(int i = a.length()-1; i >= 0; i--) {
+            finalString += a.charAt(i);
+        }
+        return finalString;
+    }
+}
