b7e471f9c731c4861474e572d015954ff501fa53
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-03-19 11:44:56 +0530

    StringManipulation

---
+++ src/StringManipulation.java
@@ -0,0 +1,12 @@
+public class StringManipulation {
+    public static void main(String[] args) {
+        String s = "Hello";
+        System.out.println(s);
+        System.out.println(s.length());
+        System.out.println(s.charAt(2));
+        System.out.println(s.toUpperCase());
+
+        System.out.println("Hello World!".length());
+
+    }
+}
