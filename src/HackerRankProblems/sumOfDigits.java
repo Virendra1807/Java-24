fc15d0e54a68d009e94262cef3ae45964acebb14
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-05-04 20:13:42 +0530

    Adding digits of number

---
+++ src/HackerRankProblems/sumOfDigits.java
@@ -0,0 +1,20 @@
+package HackerRankProblems;
+
+import java.util.Scanner;
+
+public class sumOfDigits {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number : ");
+        int num = sc.nextInt();
+        int sum = 0;
+        while (num != 0){
+            int digit = num % 10;
+            sum += digit;
+            num /= 10;
+        }
+        System.out.println(sum);
+    }
+}
