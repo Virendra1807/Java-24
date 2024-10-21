d1c9870932954b9546f4edbacaed9de28ada8572
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-10-21 20:02:07 +0530

    Selection Sort

---
+++ src/SortingAlgorithms/SelectionSort.java
@@ -0,0 +1,20 @@
+package SortingAlgorithms;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+
+        int arr[] = {10,5,1,9,6,5,2};
+
+        // Selection Sort
+
+        for (int pass = 0; pass < arr.length; pass++) {
+            for (int i = 0; i < arr.length; i++) {
+                if( arr[i] < arr[i+1]){
+
+                }
+            }
+
+        }
+    }
+}
